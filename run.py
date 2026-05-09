7f16187400865aaab11915227753839fb75dacc5
LEO <baxodirmamatvaliev@gmail.com>
2026-05-09 11:11:33 +0900

    feat: Python system variables & special method via dunders

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-a = 143
-print("a:", a)
+# Dunder - "Duble andr scol".  __bulltins__ , __init__  PYTHON ni ichki qurilish tizimi:
 
-
-message = "hello world!"
+message = "Python da barcha narsalar hammasi object hisoblanadi"
 print(message)
+
+result = type(message)
+print("result:", result)
+
+
+'''
+in Python , there are builtin tools:  ICHKI ORNATILGAN TOOLLAR MAVJUD:
+(1) TYPES > int float str list dict
+(2) FUNCTION > print() len() input() type()::  typni ozgartiradigan function: str() int()
+(3) __bulltins__ dahildor: CONSTANSTS > True False None
+
+'''
+
+print(dir(__builtins__))
