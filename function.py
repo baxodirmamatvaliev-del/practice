1d13e6e89747afa3d2d6f2a6471fd39ac79d8a71
LEO <baxodirmamatvaliev@gmail.com>
2026-05-09 13:09:16 +0900

    fix: learn keyword & default arguments

--- function.py
+++ function.py
@@ -29,3 +29,21 @@ print("result1:", result1)
 # [khan] argument:
 result2 = greeting("KHAN")
 print("result2:", result2)
+
+
+print("========= Keyword & default arguments =========")
+
+# DEFINE
+
+
+def give_greet(name, age=26):
+    print("give_greet is exsecuted")
+    return f"hii {name}, you are {age} years old!"
+
+
+result3 = give_greet(name="Leo", age=20)
+print("result3:", result3)
+
+
+result4 = give_greet("John")
+print("result4:", result4)
