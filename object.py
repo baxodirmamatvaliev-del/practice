6665b45427c96547db0e967ee0a2ef36b39c32b1
LEO <baxodirmamatvaliev@gmail.com>
2026-05-12 09:26:28 +0900

    feat: learn Pthon Object

---
+++ object.py
@@ -0,0 +1,20 @@
+
+'''
+(1) Object ozi nima
+(2) 
+'''
+
+import array  # package/method
+import math
+from math import ceil
+print("================ object ozi nima ===================")
+# object lar ozini mahsus state va method lariga ega borgan propertilarga ega !
+# Python da hamma narsa obeject !
+print(type(math))
+print(type(" hello world"))
+print(type(300))
+print(type(array))
+print(type(True))
+
+result1 = math.ceil(97.7)
+print("result1", result1)
