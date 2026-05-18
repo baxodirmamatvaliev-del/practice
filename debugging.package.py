6109288340fd767d072deeb2ba19cf7051b81e9d
LEO <baxodirmamatvaliev@gmail.com>
2026-05-19 00:40:50 +0900

    feat: pip extarnial peckages

--- debugging.package.py
+++ debugging.package.py
@@ -4,17 +4,18 @@
 (3) Debbuging
 '''
 
+from PIL import Image
 import turtle
 print("===========Python peckages & core package ========== ")
 ''' Python peckages/ Modules: core,file and extrnial'''
 # core peckages https://docs.python.org/3/library
 
-t = turtle.Turtle()
-t.shape("turtle")
-t.speed(1)
-t.circle(170)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(1)
+# t.circle(170)
 
-turtle.done
+# turtle.done
 
 my_file = open("material/message.txt", "r")
 
@@ -30,3 +31,14 @@ with open("material/message.txt", "r") as your_file:
     print("your_content", your_content)
 
 print("DONE")
+
+
+print("========== Package menager & externial peckage ==========")
+''' peckage meagers: pip pipenv npm yarn comproser brew'''
+# externial peckage https://pypi.org/
+
+
+with Image.open("material/2.jpg") as img_obj:
+    resized_img = img_obj.resize((200, 200))
+    resized_img.show()
+    resized_img.show("material/sample.png")
